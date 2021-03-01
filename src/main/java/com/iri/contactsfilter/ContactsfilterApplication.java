6bcea85bf9b3be53fe1f356cbcd5a9d47b1bd8dd
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2021-03-01 16:47:07 +0200

    first commit

---
+++ src/main/java/com/iri/contactsfilter/ContactsfilterApplication.java
@@ -0,0 +1,13 @@
+package com.iri.contactsfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsfilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsfilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iri/contactsfilter/ContactsfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iri.contactsfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsfilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
